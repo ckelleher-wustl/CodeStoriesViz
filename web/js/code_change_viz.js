bb9aa2d6642c5152a7bd9020372f3c68a8ce148e
Caitlin Kelleher <ckelleher@wustl.edu>
2023-01-10 15:12:55 -0600

    [Commit time: 1/10/2023, 3:12:54 PM]

--- web/js/code_change_viz.js
+++ web/js/code_change_viz.js
@@ -101,6 +101,7 @@ function displayCodeChangeViz() {
         var changes = d.numAdds + d.numRemoves;
         console.log("change count " + changes);
         console.log("maxWidth " + maxWidth);
+        console.log((changes)/150 * (maxWidth-15) );
 
         return( 3 + (changes)/150 * (maxWidth-15) )
     })
