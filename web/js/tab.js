f9904afbd60362123470337a2309b4bd9292a183
Caitlin Kelleher <ckelleher@wustl.edu>
2023-07-19 16:46:48 -0500

    event listener initialization for dynamic html

--- web/js/tab.js
+++ web/js/tab.js
@@ -1,41 +1,64 @@
 var initTest = false;
-var linkList = document.getElementById("linkList");
-var contentContainer = document.getElementById("contentContainer");
+// var linkList = document.getElementById("linkList");
+// var contentContainer = document.getElementById("contentContainer");
+
+var linkList = $("#linkList");
+var contentContainer = $("#contentContainer");
 
 
 function openTab(tabName) {
   var i, tabContent;
-  tabContent = document.getElementsByClassName("tab");
+  tabContent = $(".tab");
   for (i = 0; i < tabContent.length; i++) {
-    tabContent[i].style.display = "none";
+    tabContent[i].removeAttribute("style");
   }
-  document.getElementById(tabName).style.display = "block";
+  $("#"+tabName).attr("style", "display:block")
+
+  if (tabName == "history") {
+    // initCollapsibles();
+  }
 
   if (tabName == "test") {
-    linkList = document.getElementById("linkList");
-    contentContainer = document.getElementById("contentContainer");
+    linkList = $("#linkList");
+    contentContainer = $("#contentContainer");
 
-    linkList.addEventListener("click", (event) => {
-        console.log("click on link")
-        event.preventDefault();
-        const link = event.target.closest("a");
+    linkList.on("click", (event) => {
+      event.preventDefault();
+      const link = event.target.closest("a");
 
-        if (link) {
-            const url = link.getAttribute("data-url");
-            loadContent(url);
-        }
-    });
+      if (link) {
+          const url = link.getAttribute("data-url");
+          var contentContainer = $("#contentContainer");
+          console.log("click on link " + url);
+          console.log(contentContainer);
+          loadContent(url, contentContainer);
+      }
+  });
+
   }
 }
 
+function loadContent(url, container) {
+  console.log("trying to load " + url + "into " + container.attr("id"))
 
-function loadContent(url) {
   fetch(url)
-      .then((response) => response.text())
-      .then((html) => {
-      contentContainer.innerHTML = html;
-      })
-      .catch((error) => {
+    .then((response) => response.text())
+    .then((html) => {
+      
+      console.log("loading " + container.attr("id"));
+      if (container.attr("id") == "historyTab"){
+        container.html( html );
+        initCollapsibles();
+        
+      } else if (container.attr("id") == "contentContainer") {
+        console.log( html.substring(0, 300))
+        container.html(html);
+        initHistory();
+      } else {
+        container.html( html );
+      }
+    })
+    .catch((error) => {
       console.error("Error fetching content:", error);
-      });
+    });
 }
